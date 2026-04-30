55b41d56967ff3a1e80bc1aabc69b31ecaa549a5
canelasprite <crisforanime2020@gmail.com>
2026-04-30 08:47:12 -0400

    crear estructura base del proyecto

---
+++ cajero.c
@@ -0,0 +1,13 @@
+/*
+ * CAJERO AUTOMATICO DIGITAL
+ * Lenguaje: C (C99)
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    printf("CAJERO AUTOMATICO DIGITAL\n");
+    printf("Sistema en construccion...\n");
+    return 0;
+}
